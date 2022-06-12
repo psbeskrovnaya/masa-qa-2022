3e89959585d1f95ac89ce2a5fca7051a33a053b5
psbeskrovnaya <107350365+psbeskrovnaya@users.noreply.github.com>
2022-06-12 15:02:19 +0300

    My second commit

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("РУБАИ\n" +
+                "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной.\n" +
+                "Мне известно, что мне ничего не известно!\n" +
+                "Вот последняя правда, открытая мной.\n" +
+                "\t\t\t\tО. Хайям");
     }
 }
