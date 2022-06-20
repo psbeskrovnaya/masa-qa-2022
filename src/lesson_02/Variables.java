1ce7e9dbeecaac393b0961367b0681d3e2ed3811
psbeskrovnaya <107350365+psbeskrovnaya@users.noreply.github.com>
2022-06-20 20:07:42 +0300

    My commit

---
+++ src/lesson_02/Variables.java
@@ -0,0 +1,66 @@
+package lesson_02;
+
+public class Variables {
+    public static void main(String[] args) {
+
+      // One Line Initialisation
+        byte myByte = 2;
+
+      // Different letters of the same name
+      byte myBytE;
+      byte MYBYTE;
+
+      // Two Line Initialisation
+        byte myByte_2;
+        myByte_2 =3;
+
+      // myByte = 320; Triggers error
+
+      short myShort = 23432;
+
+      int myInt = 3453454;
+
+      long myLong = 123123123123123L;
+
+      float myFloat = 5.56f;
+
+      double myDouble = 5.678;
+
+      boolean myTrue = true;
+
+      boolean myFalse = false;
+
+      char myChar = 'd';
+      char myChar2 = '9';
+      char myChar3 = '\u03A9';
+
+      String myString = "Alex";
+
+      byte myByte4 = 8;
+      byte myByte5 = 9;
+
+      short myShort1 = 1232;
+      short myShort2 = 8989;
+
+      int myInt1 = 8989898;
+      int myInt2 = 6789876;
+
+      long myLong1 = 88888888888888L;
+      long myLong2 = 98765456789876L;
+
+      float myFloat1 = 8.88f;
+      float myFloat2 = 7.99f;
+
+      double myDouble1 = 8.8888;
+      double myDouble2 = 5.9676;
+
+      boolean myBoolean1 = true;
+
+
+
+      System.out.println(myByte4 + " " + myByte5 + " " + myShort1 + " " + myShort2 + " " + myInt1 + " " + myInt2 + " " + myLong1 + " " + myLong2 + " ");
+
+
+
+    }
+}
